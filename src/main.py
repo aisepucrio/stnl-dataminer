0361a07afd9c178df90b80ddeb67a09e6168f6eb
Breno Braga <141048792+brenonevs@users.noreply.github.com>
2024-07-31 19:28:01 -0300

    improv: Aprimorando a forma com que o framework é iniciado
    JIRA: [CSTONE-202]

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 import tkinter as tk
 from view.menu_view import DataMinerApp
+import os
+import sys
 from dotenv import load_dotenv
+
+# Obtém o diretório onde o script está localizado
+script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+# Muda para o diretório do script
+os.chdir(script_dir)
+
+print("O diretório do script é:", script_dir)
+
+# Código a ser executado a partir do diretório do script
+print("O diretório atual é:", os.getcwd())
 
 load_dotenv()
 
