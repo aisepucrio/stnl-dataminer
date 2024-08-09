51bf69307b6052b86c6828c5bc467fe760bd50f7
GabrielMM47 <gabrielmmendes@outlook.com>
2024-08-09 15:52:14 -0300

    view: Mudança da view para o layout grid e organização dos elementos referentes a obtenção das issues adicionais.
    JIRA: [CSTONE-202][CSTONE-225]

--- src/view/base_view.py
+++ src/view/base_view.py
@@ -14,63 +14,68 @@ class BaseView(ctk.CTk):
         self.geometry(f'{self.width}x{self.height}')
         self.configure(bg="black")
 
+        # Configura o layout grid
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+
         # Adiciona um botão de voltar para navegar de volta ao menu
         self.back_button = ctk.CTkButton(
             self, text="← Back", command=self.back_to_menu,
             corner_radius=8, fg_color="#2e2e2e", hover_color="#4a4a4a",
             text_color="#ffffff", width=80, height=32, font=("Segoe UI", 12, "bold")
         )
-        self.back_button.pack(pady=7, padx=10, anchor='nw')
+        self.back_button.grid(row=0, column=0, padx=10, pady=7, sticky='w')
 
         # Define a fonte padrão
         self.default_font = ctk.CTkFont(family="Segoe UI", size=12)
 
         # Rótulo e campo de entrada para URL
         self.url_label = ctk.CTkLabel(self, text=url_label, font=self.default_font)
-        self.url_label.pack(pady=5)
+        self.url_label.grid(row=1, column=0, columnspan=2, pady=5)
         self.url_entry = ctk.CTkEntry(self, placeholder_text=url_placeholder, width=400, font=self.default_font)
-        self.url_entry.pack(pady=7, padx=10)
+        self.url_entry.grid(row=2, column=0, columnspan=2, pady=5)
 
         # Rótulos e campos de entrada para datas
         self.start_date_label = ctk.CTkLabel(self, text="Start Date (DD/MM/YYYY)", font=self.default_font)
-        self.start_date_label.pack(pady=5, padx=10)
+        self.start_date_label.grid(row=3, column=0, columnspan=2, pady=5, padx=10)
         self.start_date_entry = DateEntry(self, date_pattern='dd/MM/yyyy', width=12, background='darkblue', foreground='white', borderwidth=2)
-        self.start_date_entry.pack(pady=7, padx=10)
+        self.start_date_entry.grid(row=4, column=0, columnspan=2, pady=10, padx=7)
 
         self.end_date_label = ctk.CTkLabel(self, text="End Date (DD/MM/YYYY)", font=self.default_font)
-        self.end_date_label.pack(pady=5, padx=10)
+        self.end_date_label.grid(row=5, column=0, columnspan=2, pady=5, padx=10, sticky='')
         self.end_date_entry = DateEntry(self, date_pattern='dd/MM/yyyy', width=12, background='darkblue', foreground='white', borderwidth=2)
-        self.end_date_entry.pack(pady=7, padx=10)
+        self.end_date_entry.grid(row=6, column=0, columnspan=2, pady=10, padx=7, sticky='')
 
         # Frame para as opções de mineração
         self.mining_options_frame = ctk.CTkFrame(self)
-        self.mining_options_frame.pack(pady=20)
+        self.mining_options_frame.grid(row=7, column=0, columnspan=2, pady=20, sticky='n')
 
         # Frame para os tipos de issue adicionais
         self.additional_issues_frame = ctk.CTkFrame(self)
-        self.additional_issues_frame.pack(pady=7, padx=10, anchor='center')
+        self.additional_issues_frame.grid(row=8, column=0, columnspan=2, pady=7, padx=10, sticky='n')
 
         # Botão para iniciar a mineração de dados
         self.mine_button = ctk.CTkButton(self, text="Mine Data", command=self.mine_data, font=self.default_font, corner_radius=8)
-        self.mine_button.pack(pady=7, padx=10)
+        self.mine_button.grid(row=9, column=0, columnspan=2, pady=7, padx=10, sticky='')
 
         # Botão para parar o processo
         self.stop_button = ctk.CTkButton(self, text="Stop", command=self.stop_process, font=self.default_font, corner_radius=8, fg_color="red")
-        self.stop_button.pack(pady=7, padx=10)
+        self.stop_button.grid(row=10, column=0, columnspan=2, pady=7, padx=10, sticky='')
 
         # Barra de progresso
         self.progress_bar = ctk.CTkProgressBar(self)
-        self.progress_bar.pack(pady=12, padx=10)
+        self.progress_bar.grid(row=11, column=0, columnspan=2, pady=12, padx=10, sticky='')
         self.progress_bar.set(0)
 
         # Rótulo para mostrar os resultados
         self.result_label = ctk.CTkLabel(self, text="", font=self.default_font)
-        self.result_label.pack(pady=12, padx=10)
+        self.result_label.grid(row=12, column=0, columnspan=2, pady=12, padx=10, sticky='n')
 
     # Função para mostrar uma mensagem temporária
     def show_temp_message(self, message, duration=3000):
         temp_label = ctk.CTkLabel(self, text=message, font=self.default_font)
-        temp_label.pack(pady=12, padx=10)
+        temp_label.grid(row=12, column=0, columnspan=2, pady=12, padx=10, sticky='n')
         self.after(duration, temp_label.destroy)
 
     # Função para centralizar a janela na tela
--- src/view/jira_view.py
+++ src/view/jira_view.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 import threading
 from controller.jira_controller import JiraController
 from view.base_view import BaseView
+from PIL import Image
 
 # Classe para a aplicação de mineração de dados do Jira
 class JiraApp(BaseView):
@@ -13,25 +14,27 @@ class JiraApp(BaseView):
         # Definir valor padrão para o campo de entrada de URL
         #self.url_entry.insert(0, "https://stone-puc.atlassian.net/jira/software/c/projects/CSTONE/boards/3?isInsightsOpen=true")
         self.url_entry.insert(0, "https://spark-project.atlassian.net/jira/software/c/projects/SPARK/issues")
+
+        # Botão para carregar tipos de issues adicionais (com ícone, se disponível)
+        #self.load_issues_icon = ctk.CTkImage(light_image=Image.open(r"view/icons/close.png"), size=(20, 20))
+        self.load_issues_button = ctk.CTkButton(self, text="", command=self.load_issue_types, width=25, height=25) # Adicionar image=self.load_issues_icon
+        self.load_issues_button.grid(row=2, column=1, padx=(5, 10), pady=7, sticky='e')
+
         # Adiciona opções de mineração específicas do Jira
         self.epics_switch = ctk.CTkSwitch(self.mining_options_frame, text="Epics", font=self.default_font)
-        self.epics_switch.pack(pady=5, padx=20, anchor='w')
+        self.epics_switch.grid(row=0, column=0, padx=20, pady=5, sticky='w')
         self.user_stories_switch = ctk.CTkSwitch(self.mining_options_frame, text="User Stories", font=self.default_font)
-        self.user_stories_switch.pack(pady=5, padx=20, anchor='w')
+        self.user_stories_switch.grid(row=1, column=0, padx=20, pady=5, sticky='w')
         self.tasks_switch = ctk.CTkSwitch(self.mining_options_frame, text="Tasks", font=self.default_font)
-        self.tasks_switch.pack(pady=5, padx=20, anchor='w')
+        self.tasks_switch.grid(row=2, column=0, padx=20, pady=5, sticky='w')
         self.subtasks_switch = ctk.CTkSwitch(self.mining_options_frame, text="Sub-tasks", font=self.default_font)
-        self.subtasks_switch.pack(pady=5, padx=20, anchor='w')
+        self.subtasks_switch.grid(row=3, column=0, padx=20, pady=5, sticky='w')
         self.bugs_switch = ctk.CTkSwitch(self.mining_options_frame, text="Bugs", font=self.default_font)
-        self.bugs_switch.pack(pady=5, padx=20, anchor='w')
+        self.bugs_switch.grid(row=4, column=0, padx=20, pady=5, sticky='w')
 
         # Frame para os tipos de issue adicionais
         self.additional_issues_label = ctk.CTkLabel(self.additional_issues_frame, text="Additional Task Types", font=self.default_font)
-        self.additional_issues_label.pack(pady=5, padx=10)
-
-        # Botão para carregar tipos de issues adicionais (com ícone, se disponível)
-        self.load_types_button = ctk.CTkButton(self, text="", command=self.load_issue_types, width=32, height=32)
-        self.load_types_button.pack(side="right", padx=5)
+        self.additional_issues_label.grid(row=0, column=0, padx=10, pady=5, sticky='w')
 
         # Lista para armazenar os estados dos CTkCheckBox
         self.additional_issues_vars = []
